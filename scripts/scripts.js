87978b535b48095b24124fb62a9afcc8c8631925
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-26 19:02:17 +0200

    feature(divs): Add container and square divs
    * Add a container div that will be the flex container.
    * Add 16 divs that will be square flex-items
    * Add generic styling for visibility

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const container = document.querySelector("#container");
+
+// Create 16 div's.
+for (let i = 0; i < 16; i++) {
+    const squareDiv = document.createElement("div");
+
+    squareDiv.textContent = "Test";
+    squareDiv.className = "squareDiv";
+    squareDiv.style.background = "green";
+    squareDiv.style.width = "25%";
+
+    container.appendChild(squareDiv);
+}
