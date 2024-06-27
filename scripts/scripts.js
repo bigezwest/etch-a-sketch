4d3a48545f63cd81b31d1867743ecd084198eeb6
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-27 02:37:01 +0200

    100vw / 25vw;

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,16 @@
 const container = document.querySelector("#container");
 
 // Create 16 div's.
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 240; i++) {
     const squareDiv = document.createElement("div");
 
-    squareDiv.textContent = "Test";
+    squareDiv.textContent = "Test"; // Temp
     squareDiv.className = "squareDiv";
-    squareDiv.style.background = "green";
-    squareDiv.style.width = "25%";
+    const divW = (100 / Math.sqrt(240)) + "vw";
+
+    squareDiv.style.width = divW;
+    squareDiv.style.height = divW;
+    // squareDiv.style.background = "green"; // Temp
 
     container.appendChild(squareDiv);
 }
